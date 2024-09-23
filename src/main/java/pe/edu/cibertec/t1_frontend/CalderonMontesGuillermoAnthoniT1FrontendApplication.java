09da895af568186687e87ce5be4e63426f3b2055
GuilleCalM <guilleacalderon@hotmail.com>
2024-09-22 23:10:16 -0500

    Commit inicial del proyecto T1_Frontend

---
+++ src/main/java/pe/edu/cibertec/t1_frontend/CalderonMontesGuillermoAnthoniT1FrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t1_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalderonMontesGuillermoAnthoniT1FrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalderonMontesGuillermoAnthoniT1FrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/cibertec/t1_frontend/CalderonMontesGuillermoAnthoniT1FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t1_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalderonMontesGuillermoAnthoniT1FrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
